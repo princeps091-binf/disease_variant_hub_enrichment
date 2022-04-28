f28c523234284fc816801303e12a998d69623da9
princeps091-binf <princeps091@gmail.com>
2022-04-28 19:49:20 +0200

    adapted rn hubs snp count to ebi-gwas data

--- code/embl_ebi_gwas_explore.R
+++ code/embl_ebi_gwas_explore.R
@@ -10,6 +10,12 @@ ebi_gwas %>%
   scale_x_log10()
 
 ebi_gwas %>% 
+  filter(!(is.na(CHR_POS))) %>% 
+  mutate(CHR_ID=fct_relevel(CHR_ID,c(1:22,"X","Y"))) %>% 
+  ggplot(.,aes(CHR_POS,CHR_ID))+
+  geom_point(size=0.01,alpha=0.01)
+
+ebi_gwas %>% 
   group_by(`DISEASE/TRAIT`) %>% 
   summarise(n=n()) %>% 
   filter(grepl("[B-b]reast ca",MAPPED_TRAIT)) %>% 
@@ -18,6 +24,8 @@ ebi_gwas %>%
   geom_density()+
   scale_x_log10()
 
+ebi_gwas %>% 
+  ggplot(.,aes(CHR_POS,CHR_ID))+geom_point()
 
 EFO_map<-vroom("~/Documents/multires_bhicect/data/epi_data/VCF/EMBL_EBI/gwas-efo-trait-mappings.tsv")
 
@@ -44,10 +52,32 @@ Cancer_efo_tbl<-Cancer_efo_tbl %>%
   mutate(EFO=str_split(`EFO URI`,"/",simplify = T)[,5])
 
 plan(multisession,workers=4)
-Cancer_efo_tbl %>%
+Cancer_efo_tbl <- Cancer_efo_tbl %>%
   mutate(n.variant=future_map_int(EFO,function(x){
   
   nrow(ebi_gwas %>% filter(grepl(x,MAPPED_TRAIT_URI)) %>% distinct)
   
 }))
 plan(sequential)
+ebi_gwas %>% 
+  filter(MAPPED_TRAIT_URI %in% Cancer_efo_tbl$`EFO URI`)
+
+ebi_gwas %>% 
+  filter(!(is.na(CHR_POS))) %>% 
+  filter(MAPPED_TRAIT_URI %in% Cancer_efo_tbl$`EFO URI`) %>% 
+  mutate(CHR_ID=fct_relevel(CHR_ID,c(1:22,"X","Y"))) %>% 
+  ggplot(.,aes(CHR_POS,CHR_ID))+
+  geom_point(size=0.1,alpha=0.2)
+
+ebi_Grange<-   GRanges(seqnames=Cancer_ebi_gwas$CHR_ID,
+                       ranges = IRanges(start=as.numeric(Cancer_ebi_gwas$CHR_POS),
+                                        end=as.numeric(Cancer_ebi_gwas$CHR_POS)
+                       ))
+
+ch = import.chain("~/Documents/multires_bhicect/data/epi_data/hg38ToHg19.over.chain")
+ebi_Grange_hg19<-unlist(liftOver(ebi_Grange, ch))
+
+library(ChIPseeker)
+library(TxDb.Hsapiens.UCSC.hg19.knownGene)
+txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
+peakAnno_ebi_cancer <- annotatePeak(ebi_Grange_hg19, tssRegion=c(-3000, 3000),TxDb=txdb, annoDb="org.Hs.eg.db",verbose = F)@annoStat
---
+++ code/embl_ebi_gwas_rn_hubs.R
@@ -0,0 +1,115 @@
+library(GenomicRanges)
+library(vroom)
+library(furrr)
+library(valr)
+library(rtracklayer)
+options(scipen = 999999999)
+res_set <- c('1Mb','500kb','100kb','50kb','10kb','5kb')
+res_num <- c(1e6,5e5,1e5,5e4,1e4,5e3)
+names(res_num)<-res_set
+#-------------------------------------------------------------------------------------------------------
+tbl_in_fn<-function(tmp_file){
+  tmp_tbl<-get(base::load(tmp_file))
+  tmp_obj<-names(mget(base::load(tmp_file)))
+  rm(list=tmp_obj)
+  rm(tmp_obj)
+  
+  return(tmp_tbl)
+}
+cl_reduce_coord_fn<-function(hmec_dagger_01_tbl,tmp_res,res_num){
+  
+  tmp_bin_tbl<-hmec_dagger_01_tbl %>% filter(res==tmp_res) %>%unnest(cols = "bins")
+  tmp_bin_tbl<-tmp_bin_tbl%>%mutate(end = as.numeric(bins) + res_num[res]-1)
+  tmp_bin_tbl<-tmp_bin_tbl%>%distinct(chr,bins,end)
+  inter_cl_Grange<-   GRanges(seqnames=tmp_bin_tbl$chr,
+                              ranges = IRanges(start=as.numeric(tmp_bin_tbl$bins),
+                                               end=tmp_bin_tbl$end,
+                                               names=paste("cl_inter",1:nrow(tmp_bin_tbl),sep='_')
+                              ))
+  inter_cl_Grange<-IRanges::reduce(inter_cl_Grange)
+  return(inter_cl_Grange)
+  
+}
+
+hub_sets_GRange_build_fn<-function(union_cl_file,union_hub_file){
+  cl_union_tbl<-tbl_in_fn(union_cl_file)
+  hmec_dagger_01_tbl<-tbl_in_fn(union_hub_file) #%>% filter(res == "5kb" | res == "10kb")
+  hmec_dagger_01_tbl<-hmec_dagger_01_tbl %>% left_join(.,cl_union_tbl %>% dplyr::rename(node=cl)%>% dplyr::select(chr,node,res,bins))
+  rm(cl_union_tbl)
+  
+  hub_GRange<-do.call("c",lapply(unique(hmec_dagger_01_tbl$res),function(f){
+    tmp<-cl_reduce_coord_fn(hmec_dagger_01_tbl,f,res_num)
+    mcols(tmp)<-tibble(res=f)
+    return(tmp)
+  }))
+  return(hub_GRange)
+}
+#-------------------------------------------------------------------------------------------------------
+# table with cluster of interest
+union_hub_files<-c(H1="~/Documents/multires_bhicect/Bootstrapp_fn/data/DAGGER_tbl/trans_res/H1_union_top_trans_res_dagger_tbl.Rda",
+                   HMEC="~/Documents/multires_bhicect/Bootstrapp_fn/data/DAGGER_tbl/trans_res/HMEC_union_top_trans_res_dagger_tbl.Rda",
+                   GM12878="~/Documents/multires_bhicect/Bootstrapp_fn/data/DAGGER_tbl/trans_res/GM12878_union_top_trans_res_dagger_tbl.Rda")
+
+union_cl_files<-c(H1="~/Documents/multires_bhicect/Bootstrapp_fn/data/pval_tbl/CAGE_union_H1_pval_tbl.Rda",
+                  HMEC="~/Documents/multires_bhicect/Bootstrapp_fn/data/pval_tbl/CAGE_union_HMEC_pval_tbl.Rda",
+                  GM12878="~/Documents/multires_bhicect/Bootstrapp_fn/data/pval_tbl/CAGE_union_GM12878_pval_tbl.Rda")
+
+#-------------------------------------------------------------------------------------------------------
+# Spectral clustering results
+
+hub_GRanges_l<-lapply(names(union_hub_files),function(i){
+  message(i)
+  tmp_union_cl_file<-union_cl_files[i]
+  tmp_union_hub_file<-union_hub_files[i]
+  return(IRanges::reduce(hub_sets_GRange_build_fn(tmp_union_cl_file,tmp_union_hub_file)))
+})
+names(hub_GRanges_l)<-names(union_hub_files)
+#-------------------------------------------------------------------------------------------------------
+#Load EBI-GWAS data
+ebi_gwas<-vroom("~/Documents/multires_bhicect/data/epi_data/VCF/EMBL_EBI/gwas_catalog_v1.0.2-associations_e105_r2022-04-07.tsv")
+
+EFO_map<-vroom("~/Documents/multires_bhicect/data/epi_data/VCF/EMBL_EBI/gwas-efo-trait-mappings.tsv")
+Cancer_efo_tbl<-EFO_map %>% 
+  filter(`Parent term` == "Cancer") %>% 
+  group_by(`EFO term`) %>% 
+  dplyr::select(`EFO term`,`EFO URI`) %>% distinct
+
+Cancer_efo_tbl<-Cancer_efo_tbl %>% 
+  mutate(EFO=str_split(`EFO URI`,"/",simplify = T)[,5])
+
+Cancer_ebi_gwas<-ebi_gwas %>% 
+  filter(MAPPED_TRAIT_URI %in% Cancer_efo_tbl$`EFO URI`) %>% 
+  filter(!(is.na(CHR_POS))) %>% 
+  mutate(CHR_ID=paste0("chr",CHR_ID))
+  
+ebi_Grange<-   GRanges(seqnames=Cancer_ebi_gwas$CHR_ID,
+                            ranges = IRanges(start=as.numeric(Cancer_ebi_gwas$CHR_POS),
+                                             end=as.numeric(Cancer_ebi_gwas$CHR_POS)
+                            ))
+
+ch = import.chain("~/Documents/multires_bhicect/data/epi_data/hg38ToHg19.over.chain")
+ebi_Grange_hg19<-unlist(liftOver(ebi_Grange, ch))
+
+hg19_coord <- read_delim("~/Documents/multires_bhicect/data/hg19.genome", 
+                         "\t", escape_double = FALSE, col_names = FALSE, 
+                         trim_ws = TRUE)
+names(hg19_coord)<-c("chrom","size")
+
+tmp_cl_tbl<-hub_GRanges_l[[2]] %>% as_tibble %>% dplyr::select(seqnames,start,end)%>%dplyr::rename(chrom=seqnames)
+gap_bed<-read_delim("~/Documents/multires_bhicect/data/gap.bed", 
+                    "\t", escape_double = FALSE, col_names = F, 
+                    trim_ws = TRUE) %>% 
+  dplyr::select(X2,X3,X4) %>% 
+  dplyr::rename(chrom=X2,start=X3,end=X4)
+  
+plan(multisession,workers=4)
+rn_SNP_count<-future_map_int(1:5e2,function(x){
+  rn_pol<-bed_shuffle(tmp_cl_tbl,genome = hg19_coord,excl = gap_bed,max_tries=1e8,within=T)
+  rn_GRange<-GRanges(seqnames=rn_pol$chrom,
+                     ranges = IRanges(start=rn_pol$start,
+                                      end=rn_pol$end))
+  return(length(unique(subjectHits(findOverlaps(rn_GRange,ebi_Grange_hg19)))))
+})
+plan(sequential)
+
+length(unique(subjectHits(findOverlaps(hub_GRanges_l[[2]],ebi_Grange_hg19))))
